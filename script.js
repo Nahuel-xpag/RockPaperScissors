89a7d9f03669ebdb6e2dd2c5f97875e18f39100e
nahuel-xpg <agustin.nahuu@gmail.com>
2023-08-27 20:39:17 -0300

    feat: added basic user interface

--- script.js
+++ script.js
@@ -1,62 +1,85 @@
+//functions
 let choices = ["rock", "paper", "scissors"]
 function getComputerChoice(){
 
     return choices[Math.floor(Math.random() * 3)];
 }
-
 function playRound (playerSelection, computerSelection) { 
     if(playerSelection === null) {
         return "You canceled"
     }
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === "rock" && computerSelection === "paper") {
-        
-        return "You Lose! Paper beats Rock"
+        outcomeContainer.appendChild(rVP);
+        cpuScore++;
+        //score.appendChild(cpuPoints)
     }else if (playerSelection === "rock" && computerSelection === "scissors") {
-        youWin = true;
-        return "You Win!"
+        outcomeContainer.appendChild(userWin);
+        userScore++
+        //score.appendChild(userPoints)
     }else if (playerSelection === "paper" && computerSelection === "scissors"){
-       
-        return "You Lose! Scissors beat Paper"
+        outcomeContainer.appendChild(sVP);
+        cpuScore++;
+        //score.appendChild(cpuPoints);
     }else if (playerSelection === "paper" && computerSelection === "rock"){
-        
-        return "You Win!"
+        outcomeContainer.appendChild(userWin);
+        userScore++;
+        //score.appendChild(userPoints)
     }else if (playerSelection === "scissors" && computerSelection === "paper"){
-       
-        return "You Win!"
+        outcomeContainer.appendChild(userWin);
+        userScore++;
+        //score.appendChild(userPoints)
     }else if (playerSelection === "scissors" && computerSelection === "rock"){
+        outcomeContainer.appendChild(pVR)
+        cpuScore++;
         
-        return "You Lose! Rock beats Paper"
     }else if (playerSelection === computerSelection) {
-        return "It's a Tie!"
+        outcomeContainer.appendChild(tie)
     }else {return "Enter a valid option"}
 }
-function game() {
-let userScore = 0;
-let pcScore = 0;
-while (userScore < 3 && pcScore < 3) {
 
-    let playerSelection = prompt();
-    let result = playRound(playerSelection, getComputerChoice());
-
-    if (result === "You Win!") {
-        userScore++;
-    } else if (result.includes("Lose")) {
-        pcScore++;
+function handleChange(){
+    if (userScore === 5){
+        score.textContent = "You are the champion!"
+        userScore = 0;
+        cpuScore = 0;
+    }else if (cpuScore === 5) {
+        score.textContent = "Cpu wins :("
+        userScore = 0;
+        cpuScore = 0;
+    }else{
+        score.textContent = `
+        You: ${userScore}
+        PC: ${cpuScore}
+      `
     }
-
-    console.log(result)
-    console.log("Your Score:" + userScore)
-    console.log("PC Score:" + pcScore)
-    };
-
-
- if (userScore === 3) {
-    console.log("You are the Champion!")
- }else if (pcScore === 3){
-    console.log("PC wins")
- }
+  
 }
-console.log(game())
 
+const outcomeContainer = document.querySelector('#outcome');
+//outcome variables
+const userWin = document.createElement('h2');
+userWin.textContent = 'You Win!';
+const tie = document.createElement('h2');
+tie.textContent = "It's a tie!";
+
+const sVP = document.createElement('h2');
+sVP.textContent = "You Lose! scissors beat paper";
+
+const pVR = document.createElement('h2');
+pVR.textContent = "You Lose! Rock beats Paper"
+
+const rVP = document.createElement('h2');
+rVP.textContent = "You lose! Paper beats Rock";
+
+
+const btns = document.querySelectorAll('button');
+
+//score
+let userScore = 0;
+let cpuScore = 0;
+
+const score = document.querySelector('#score')
+btns.forEach(btn => btn.addEventListener('click', () => playRound(btn.classList.value, getComputerChoice())));
+btns.forEach(btn => btn.addEventListener('click', () => handleChange()));
 
